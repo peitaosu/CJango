b45928d8a6ce64eda2fde2f5de3bcacffd976c9c
Tony Su <peitaosu@163.com>
2020-03-19 22:18:16 +0800

    init project

---
+++ cjango/views.py
@@ -0,0 +1,7 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.conf import settings
+from django.shortcuts import redirect
+
+def index(request):
+    return render(request, "index.html")
