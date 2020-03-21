e9bb564f3324bafb0ca83122f2a3d245bd69b877
Tony Su <peitaosu@163.com>
2020-03-21 21:37:57 +0800

    chart OOP

---
+++ cjango/charts.py
@@ -0,0 +1,46 @@
+class ChartDataset():
+    def __init__(self, label, data, backgroundColor, borderColor):
+        self.label = ""
+        self.data = []
+        self.backgroundColor = []
+        self.borderColor = []
+    
+    def export(self):
+        return {
+            "label": self.label,
+            "data": self.data,
+            "backgroundColor": self.backgroundColor,
+            "borderColor": self.borderColor
+        }
+
+class ChartOptions():
+    def __init__(self, maintainAspectRatio):
+        self.maintainAspectRatio = maintainAspectRatio
+    
+    def export(self):
+        return {
+            "maintainAspectRatio" = maintainAspectRatio
+        }
+
+class Chart():
+    def __init__(self):
+        self.type = "bar"
+        self.data = {
+            "labels": []
+            "datasets": []
+        }
+        self.options = {}
+    
+    def add_dataset(self, dataset):
+        self.data["datasets"].append(dataset)
+    
+    def update_options(self, options):
+        self.options.update(options)
+
+    def export(self):
+        return {
+            "type": self.type,
+            "data": self.data,
+            "options": self.options
+        }
+
